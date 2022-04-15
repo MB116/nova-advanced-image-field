063132cdf94e113c05659ade5dc4955876757296
Pavel Andreichenko <pashtet.mb116@gmail.com>
2022-04-15 10:19:41 +0600

    setCropBoxData changed to img-style

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -28,7 +28,7 @@
         :crop-box-resizable="false"
         dragMode="move"
         :zoomable="true"
-        :img-style="{ width: '400px', height: '300px' }"
+        :img-style="{ width: '900px', height: '506px' }"
       ></vue-cropper>
 
       <p v-if="imgSrc" class="mt-3 mb-6 flex items-center text-sm">
